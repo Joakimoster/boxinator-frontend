303a80b3053cfbdc3f369c42728453503daaae97
Joakim Österberg <Joakimosterberg@hotmail.com>
2022-06-05 10:16:53 +0200

    First form draft created

---
+++ src/components/form/CountryDropDown.js
@@ -0,0 +1,15 @@
+function CountryDropDown() {
+    return(
+        <div>
+            <label>Country</label>
+            <select>
+                <option value="Sweden">Sweden</option>
+                <option value="China">China</option>
+                <option value="Brazil">Brazil</option>
+                <option value="Australia">Australia</option>
+            </select>
+        </div>
+    )
+}
+
+export default CountryDropDown
--- src/views/BoxFormView.js
+++ src/views/BoxFormView.js
@@ -1,8 +1,34 @@
+import CountryDropDown from "../components/form/CountryDropDown"
+
 function BoxFormView() {
+
     return (
-        <main className="ListDispatches">
-            <h1>Add new box</h1>
-        </main>
+        <form>
+
+            <h1>Add a new box</h1>
+            <p>Welcome to boxinator application!</p>
+
+            <div>
+                <label>Name</label>
+                <input type="text" id="name"></input>
+            </div>
+
+            <div>
+                <label>Weight</label>
+                <input type="text" id="weight"></input>
+            </div>
+
+            <div>
+                <label>Box colour</label>
+                <input type="color" id="color"></input>
+            </div>
+
+            <div>
+                <CountryDropDown/>
+            </div>
+            
+            <button type="submit">Save</button>
+        </form>
     )
 }
 
