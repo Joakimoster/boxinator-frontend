2ef504e922caaa693040a57f9d4515eee8766ded
Joakim Österberg <Joakimosterberg@hotmail.com>
2022-06-05 20:34:23 +0200

    Added mocked local state

--- src/views/BoxFormView.js
+++ src/views/BoxFormView.js
@@ -1,6 +1,21 @@
+import { useState } from "react"
 import CountryDropDown from "../components/form/CountryDropDown"
 
 function BoxFormView() {
+
+    const [ formData, setFormData ] = useState({
+        name: '',
+        weight: '',
+        color: '',
+        select: ''
+    });
+
+    const onInputChange = event => {
+        setFormData({
+            ...formData,
+            [event.target.id]: event.target.value
+        })
+    }
 
     return (
         <form>
@@ -10,21 +25,42 @@ function BoxFormView() {
 
             <div>
                 <label>Name</label>
-                <input type="text" id="name"></input>
+                <input
+                    type="text"
+                    id="name"
+                    onChange={onInputChange}
+                >
+                </input>
             </div>
 
             <div>
                 <label>Weight</label>
-                <input type="text" id="weight"></input>
+                <input
+                    type="text"
+                    id="weight"
+                    onChange={onInputChange}
+                >
+                </input>
             </div>
 
             <div>
                 <label>Box colour</label>
-                <input type="color" id="color"></input>
+                <input
+                    type="color"
+                    id="color"
+                    onChange={onInputChange}
+                >
+                </input>
             </div>
 
             <div>
-                <CountryDropDown/>
+                <label>Country</label>
+                <select id="dropdown" onChange={onInputChange}>
+                    <option value="Sweden">Sweden</option>
+                    <option value="China">China</option>
+                    <option value="Brazil">Brazil</option>
+                    <option value="Australia">Australia</option>
+                </select>
             </div>
             
             <button type="submit">Save</button>
